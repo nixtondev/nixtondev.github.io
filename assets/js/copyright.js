870d0e601e5242c9370d64cb7a38cde38815ff9b
~$ sudo <83667327+shadboyme@users.noreply.github.com>
2021-05-14 07:03:32 +0200

    Add files via upload

---
+++ assets/js/copyright.js
@@ -0,0 +1,9 @@
+var copy = "&copy;";
+var author = " shadboy.me";
+
+function getCopy(year){
+document.write(copy);
+document.write(author);
+document.write(year, ' - ');
+document.write(new Date().getFullYear());
+}
