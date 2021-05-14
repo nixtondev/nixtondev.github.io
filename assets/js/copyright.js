4644a898568d15409f6a3228f2133c3e150b0836
~$ sudo <83667327+shadboyme@users.noreply.github.com>
2021-05-14 07:01:22 +0200

    Add files via upload

---
+++ assets/js/copyright.js
@@ -0,0 +1,9 @@
+var copy = "&copy;";
+var author = " Alexander Lehresjön";
+
+function getCopy(year){
+document.write(copy);
+document.write(author);
+document.write(year, ' - ');
+document.write(new Date().getFullYear());
+}
