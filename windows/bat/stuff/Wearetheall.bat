f1bfacaf124d8de2c4a698b0a9b3520530970774
~$ Root <83667327+shadboyme@users.noreply.github.com>
2021-10-21 19:25:24 +0200

    Add files via upload

---
+++ windows/bat/stuff/Wearetheall.bat
@@ -0,0 +1,82 @@
+@echo off
+break off 
+
+title We are the all 
+color 0a
+mode 1000
+
+:cmd
+
+echo MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+echo MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+echo MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+echo MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMmdhysso+//:--..````````````..--:/+osyhdmNMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+echo MMMMMMMMMMMMMMMMMMMMMMmhs+/-.``                                      ```-:+sydNMMMMMMMMMMMMMMMMMMMMM
+echo MMMMMMMMMMMMMMMMMmy+:``                                                      ``./sdMMMMMMMMMMMMMMMMM
+echo MMMMMMMMMMMMMMms-`                                                                `./hMMMMMMMMMMMMMM
+echo MMMMMMMMMMMMm+`                                                                       -yMMMMMMMMMMMM
+echo MMMMMMMMMMMh.                                                                           /NMMMMMMMMMM
+echo MMMMMMMMMMh`                                                                             /MMMMMMMMMM
+echo MMMMMMMMMm`                                                                               oMMMMMMMMM
+echo MMMMMMMMM/                                                                                `mMMMMMMMM
+echo MMMMMMMMd                                                                                  +MMMMMMMM
+echo MMMMMMMM/                                                                                  `NMMMMMMM
+echo MMMMMMMN`            `.------.``                                    `.-:///::-.`            sMMMMMMM
+echo MMMMMMMy         ./sdmmNNNNNNNmdhs/-`                          `-/sdmNNMMMMMMNmdy+-         :MMMMMMM
+echo MMMMMMM:      `/hmmmdddddmmNMMMMMMMNmy/.                    `:sdNMMMMMMMNmmdddddmNNh/`      `MMMMMMM
+echo MMMMMMM`     :hdo-.``.yddddhhdmmMMMMMMMms-                .omMMMMMMMmdhhhddddy.``.-odd/      dMMMMMM
+echo MMMMMMd     oy:`      -/oyhmNMmdyydNMMMMMNs`            `omMMMMMMmhydmNMmdhs/-       -so`    yMMMMMM
+echo MMMMMMs    .-              `-odMMmyohNMMMMMd            hMMMMMNdshmMMms:.`             ..    oMMMMMM
+echo MMMMMM+                       `:yNMMdsymMMMN`           dMMMmyohNMNh/`                       /MMMMMM
+echo MMMMMM:                          -smMMmsoss-            .ss/+dMMNy:                          :MMMMMM
+echo MMMMMM.                            .omMMmo`               :dMMNs-                            .MMMMMM
+echo MMMMMM`                              .dMMMd.             oNMmo.                              `MMMMMM
+echo MMMMMM            `.:+ossssso/:.`     `dMMMm.           :MNo.      `./+ssyyyys+/-`            MMMMMM
+echo MMMMMM         `/hmMMMMMMMMMMMMNds:.   `dMMMh           .+.     `:sdMMMMMMMMMMMMMmh+.         NMMMMM
+echo MMMMMN       `+mMMMMMMMMMMMMMMMMMMMNh+` sMMMM:               `/hNMMMMMMMMMMMMMMMMMMMNo`       mMMMMM
+echo MMMMMN    .oymMMMMMMMMMMMMMMMMMMMMMMMMm.sMMMMy              `dMMMMMMMMMMMMMMMMMMMMMMMMmyo/.`  mMMMMM
+echo MMMMMm    .NMMMMMMMMMMMMMMMMMMMMMNNNNd+ sMMMMm               :hNNNNNNNNNNNNNNNmmmmmddhsssyhhysNMMMMM
+echo MMMMMm    .NMNNdhyssooo+++////::--..`   yMMMMm                 `..`````                      `mMMMMM
+echo MMMMMm    //-`                          hMMMMm                                                mMMMMM
+echo MMMMMm                                  dMMMMm                                                NMMMMM
+echo MMMMMM+                                `MMMMMm                                                NMMMMM
+echo MMMMMMN-                               /MMMMMd                                                MMMMMM
+echo MMMMMMMN.                              hMMMMMh                                               /MMMMMM
+echo MMMMMMMMN-                            `MMMMMMh                                              .NMMMMMM
+echo MMMMMMMmMM+                           sMMMMMMy                                             +NMMMMMMM
+echo MMMMMMM-:mMd:                       :hMMMMMMMs             `                            `+mMMMMMMMMM
+echo MMMMMMMh .mMMdo-          ``..-. -yNMMMMMMMMM+            +Nd+`                       -oNh:MMMMMMMMM
+echo MMMMMMMM/ -NMMMMNmmddmmmNmdhys+..NMMMMmyMMMMM.             `-sN:     :++//::---://+shmMMm`/MMMMMMMMM
+echo MMMMMMMMN. /MMN+NMMM:``````     :MMMMy``MMMMm                 sN`    `:+ossyyhmMMMMmmMMN- dMMMMMMMMM
+echo MMMMMMMMMd` +MMo-NMMm:           hMMd  .MMMMs                 `s             .mMMMy`yMM/ :MMMMMMMMMM
+echo MMMMMMMMMMy  +MN:-NMMNs`         `+h+  /MMMM/                               /NMMMo `NMs  dMMMMMMMMMM
+echo MMMMMMMMMMMs  :NN:-mMMMmo.             .yMMM.                             .yMMMN/  hMy  oMMMMMMMMMMM
+echo MMMMMMMMMMMMo  -dN+.dMMMMNdo:`           yMM/         -+:               .oNMMMN: `hMh` :MMMMMMMMMMMM
+echo MMMMMMMMMMMMMo  `yMy-sNMMMMMNmds/-`   `-yNMMNds+/://odNMNy.         `-+hNMMMMm- :mMy` -NMMMMMMMMMMMM
+echo MMMMMMMMMMMMMMo   +NNo:sNMMMMMMMMNmmddmNMMMMMMMMMNMMMMMMMMNy/:-:/+sdmNMMMMMNs`.yNMs` .mMMMMMMMMMMMMM
+echo MMMMMMMMMMMMMMMo   :mMm+-/hmMMMMMMMMMMMMMMMMMMMh--yNMMMMMMMMMNNMMMMMMMMMMms-`+mMN+  -mMMMMMMMMMMMMMM
+echo MMMMMMMMMMMMMMMMs`  .dMMd/`.:ohmMMMMMMMMMMMMMm/`   -yMMMMMMMMMMMMMMMMMmy/. :hMMN:  :mMMMMMMMMMMMMMMM
+echo MMMMMMMMMMMMMMMMMy`  `yMMMd/`  .-/oydmNNMMMMd-````  `:mMMMMMMMMMNmhs/-`   :-hMd-  /NMMMMMMMMMMMMMMMM
+echo MMMMMMMMMMMMMMMMMMh.  `oMMMMd+.     ``.----/osyyyyyyyyhdhyso+/:-.`        `yMy`  +NMMMMMMMMMMMMMMMMM
+echo MMMMMMMMMMMMMMMMMMMm-   +NMMMMms-`             ````...``                 `hN+  `sMMMMMMMMMMMMMMMMMMM
+echo MMMMMMMMMMMMMMMMMMMMN:   :NMMMMNys+:-`                                  .dd-  .hMMMMMMMMMMMMMMMMMMMM
+echo MMMMMMMMMMMMMMMMMMMMMN+   :NMMMd  .-:+o+/:--.........`                 -my`  :mMMMMMMMMMMMMMMMMMMMMM
+echo MMMMMMMMMMMMMMMMMMMMMMMs`  -NMMd       `-:+smNmmmmmmmmho`             +m+   +NMMMMMMMMMMMMMMMMMMMMMM
+echo MMMMMMMMMMMMMMMMMMMMMMMMh.  -NMh            -NMMMMMMMMd-            `sd-  `sMMMMMMMMMMMMMMMMMMMMMMMM
+echo MMMMMMMMMMMMMMMMMMMMMMMMMm-  :Nm`            oMMMMMMMd`            `ys`  .dMMMMMMMMMMMMMMMMMMMMMMMMM
+echo MMMMMMMMMMMMMMMMMMMMMMMMMMN/  /No            sMMMMMMMo             //   :mMMMMMMMMMMMMMMMMMMMMMMMMMM
+echo MMMMMMMMMMMMMMMMMMMMMMMMMMMMs` :o           :NMMMMMMMh                `oNMMMMMMMMMMMMMMMMMMMMMMMMMMM
+echo MMMMMMMMMMMMMMMMMMMMMMMMMMMMMm/            .NMMMMMMMMM+              .hMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+echo MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMy`          oMMMMMdmMMMd             :mMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+echo MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMm-         +MMMMMmmMMMm           `oMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+echo MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMN+        .MMMMMMMMMMy          -hMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+echo MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMy`       hMMMMMMMMM:        `oNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+echo MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMm/      -MMMMMMMMd       `/mMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+echo MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMd/`    dMMMMMMM:     `+mMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+echo MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMms/.`sMMMMMMM` `-+hMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+echo MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMdmMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+echo MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+echo MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+
+pause >nul 
+goto cmd 
