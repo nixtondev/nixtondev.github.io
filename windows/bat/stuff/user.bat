34d6c2187c132e17f12605416412c909045bc4f1
~$ Root <83667327+shadboyme@users.noreply.github.com>
2021-10-23 15:56:52 +0200

    Downloading up .bat files...
    My prank hacking tools....

---
+++ windows/bat/stuff/user.bat
@@ -0,0 +1,284 @@
+@echo off 
+break off
+title %userprofile% 
+color 0a
+cls 
+echo L 
+ping localhost -n .15 >nul 
+cls 
+echo Lo 
+ping localhost -n .15 >nul 
+cls 
+echo Loa 
+ping localhost -n .15 >nul 
+cls 
+echo Load 
+ping localhost -n .15 >nul 
+cls 
+echo Loadi 
+ping localhost -n .15 >nul 
+cls 
+echo Loadin 
+ping localhost -n .15 >nul 
+cls 
+echo Loading 
+ping localhost -n .15 >nul 
+cls 
+echo Loading 
+ping localhost -n .15 >nul 
+cls 
+echo Loading 
+ping localhost -n .15 >nul 
+cls 
+echo Loading s 
+ping localhost -n .15 >nul 
+cls 
+echo Loading sy 
+ping localhost -n .15 >nul 
+cls
+echo Loading sys
+ping localhost -n .15 >nul 
+cls
+echo Loading syst
+ping localhost -n .15 >nul 
+cls
+echo Loading syste
+ping localhost -n .15 >nul 
+cls
+echo Loading system
+ping localhost -n .15 >nul 
+cls
+echo Loading system n
+ping localhost -n .15 >nul 
+cls
+echo Loading system no 
+ping localhost -n .15 >nul 
+cls
+echo Loading system now
+ping localhost -n .15 >nul 
+cls
+echo Loading system now.
+ping localhost -n .15 >nul 
+cls 
+echo Loading system now.. 
+ping localhost -n .15 >nul 
+cls
+echo Loading system now... 
+ping localhost -n 5 >nul 
+cls 
+echo W 
+ping localhost -n .15 >nul 
+cls 
+echo We 
+ping localhost -n .15 >nul 
+cls 
+echo Wel 
+ping localhost -n .15 >nul 
+cls 
+echo Welc 
+ping localhost -n .15 >nul 
+cls 
+echo Welco 
+ping localhost -n .15 >nul 
+cls 
+echo Welcom 
+ping localhost -n .15 >nul 
+cls 
+echo Welcome 
+ping localhost -n .15 >nul 
+cls 
+echo Welcome B 
+ping localhost -n .15 >nul 
+cls 
+echo Welcome Ba 
+ping localhost -n .15 >nul 
+cls 
+echo Welcome Bac 
+ping localhost -n .15 >nul 
+cls 
+echo Welcome Back u 
+ping localhost -n .15 >nul 
+cls 
+echo Welcome Back us
+ping localhost -n .15 >nul 
+cls 
+echo Welcome Back use 
+ping localhost -n .15 >nul 
+cls 
+echo Welcome Back user 
+ping localhost -n .15 >nul 
+cls 
+echo Welcome Back user. 
+ping localhost -n .15 >nul 
+cls 
+echo Welcome Back user.. 
+ping localhost -n 5 >nul 
+cls 
+echo H 
+ping localhost -n .15 >nul 
+cls 
+echo Ha 
+ping localhost -n .15 >nul 
+cls 
+echo Hac 
+ping localhost -n .15 >nul 
+cls 
+echo Hack 
+ping localhost -n .15 >nul 
+cls 
+echo Hacki 
+ping localhost -n .15 >nul 
+cls 
+echo Hackin 
+ping localhost -n .15 >nul 
+cls 
+echo Hacking 
+ping localhost -n .15 >nul 
+cls 
+echo Hacking S 
+ping localhost -n .15 >nul 
+cls 
+echo Hacking Sy 
+ping localhost -n .15 >nul 
+cls 
+echo Hacking Sys 
+ping localhost -n .15 >nul 
+cls 
+echo Hacking Syst 
+ping localhost -n .15 >nul 
+cls 
+echo Hacking Syste 
+ping localhost -n .15 >nul 
+cls 
+echo Hacking System 
+ping localhost -n .15 >nul 
+cls 
+echo Hacking System N 
+ping localhost -n .15 >nul 
+cls 
+echo Hacking System No 
+ping localhost -n .15 >nul 
+cls 
+echo Hacking System Now 
+ping localhost -n 5 >nul 
+cls 
+echo Start prank software?
+pause ->nul 
+ping localhost -n .15 >nul 
+echo ud$$$**$$$$$$$bc. 
+echo u@**" 4$$$$$$$Nu 
+echo J ""#$$$$$$r 
+echo @ $$$$b MD SYED SHANIZAM 
+echo .F ^*3$$$ 
+echo :% 4 J$$$N 
+echo $ :F :$$$$$ 
+echo 4F 9 J$$$$$$$ 
+echo 4$ k 4$$$$bed$$$$$$$$$ 
+echo $$r 'F $$$$$$$$$$$$$$$$$r 
+echo $$$ b. $$$$$$$$$$$$$$$$$N 
+echo $$$$$k 3eeed$$b $$$Euec."$$$$$$$$$ 
+echo .@$**N. $$$$$" $$$$$$F'L $$$$$$$$$$$ $$$$$$$ 
+echo :$$L 'L $$$$$ 4$$$$$$ * $$$$$$$$$$F $$$$$$F edNc 
+echo @$$$$N ^k $$$$$ 3$$$$*% $F4$$$$$$$ $$$$$" d" z$N 
+echo $$$$$$ ^k '$$$" #$$$F .$ $$$$$c.u@$$$ J" @$$$$r 
+echo $$$$$$$b *u ^$L $$ $$$$$$$$$$$$u@ $$ d$$$$$$ 
+echo ^$$$$$$. "NL "N. z@* $$$ $$$$$$$$$$$$$P $P d$$$$$$$ 
+echo ^"*$$$$b '*L 9$E 4$$$ d$$$$$$$$$$$" d* J$$$$$r 
+echo ^$$$$u '$. $$$L "#" d$$$$$$".@$$ .@$" z$$$$*" 
+echo ^$$$$. ^$N.3$$$ 4u$$$$$$$ 4$$$ u$*" z$$$" 
+echo '*$$$$$$$$ *$b J$$$$$$$b u$$P $" d$$P 
+echo #$$$$$$ 4$ 3*$"$*$ $"$'c@@$$$$ .u@$$$P 
+echo "$$$$ ""F~$ $uNr$$$^&J$$$$F $$$$# 
+echo "$$ "$$$bd$.$W$$$$$$$$F $$" 
+echo ?k ?$$$$$$$$$$$F'* 
+echo 9$$bL z$$$$$$$$$$$F 
+echo $$$$ $$$$$$$$$$$$$ 
+echo '#$$c '$$$$$$$$$" 
+echo .@"#$$$$$$$$$$$$b 
+echo z* $$$$$$$$$$$$N. 
+echo e" z$$" #$$$k '*$$. 
+echo .u* u@$P" '#$$c "$$c 
+echo u@$*""" d$$" "$$$u ^*$$b. 
+echo :$F J$P" ^$$$c '"$$$$$$bL 
+echo d$$ .. @$# #$$b '#$ 
+echo 9$$$$$$b 4$$ ^$$k '$ 
+echo "$$6""$b u$$ '$ d$$$$$P 
+echo '$F $$$$$" ^b ^$$$$b$ 
+echo '$W$$$$" 'b@$$$$" 
+echo Downloading Virus Now!
+echo Downloading Virus Now!
+echo Downloading Virus Now! 
+echo 
+ping localhost -n 5 >nul 
+cls 
+echo L 
+ping localhost -n .15 >nul 
+cls 
+echo Lo 
+ping localhost -n .15 >nul 
+cls 
+echo Loa 
+ping localhost -n .15 >nul 
+cls 
+echo Load 
+ping localhost -n .15 >nul 
+cls 
+echo Loadi 
+ping localhost -n .15 >nul 
+cls 
+echo Loadin 
+ping localhost -n .15 >nul 
+cls 
+echo Loading 
+ping localhost -n .15 >nul 
+cls 
+echo Loading v
+ping localhost -n .15 >nul 
+cls 
+echo Loading vi
+ping localhost -n .15 >nul 
+cls
+echo Loading vir
+ping localhost -n .15 >nul 
+cls 
+echo Loading viru
+ping localhost -n .15 >nul 
+cls 
+echo Loading virus
+ping localhost -n .15 >nul 
+cls 
+echo Loading virus n
+ping localhost -n .15 >nul 
+cls 
+echo Loading virus no
+ping localhost -n .15 >nul 
+cls 
+echo Loading virus now
+ping localhost -n .15 >nul 
+cls 
+cls 
+echo Loading virus now. 
+ping localhost -n 5 >nul 
+cls 
+:start 
+echo 010 010 10 01 0100 10 010 10 010 1001 10 010 10 10 010 101110 10 100 100 1011 
+echo 101010100 100 00100 010 0010 01000010 010 01000100010 01000 0100100 100 01000 
+echo 101 0100010 1000001 01010 10 0100010 10 0010 010010 0 010 010 0000111001011011 
+echo 000 010 01010010 010001001 00100100 1010 01001001 0010010 01001000100 00100111 
+echo 001 0010 10 010010010 010 100 10 01 010 010 010 010 10 010 10 010 10 01010 0 
+echo 000 010 00 001 0010 00 1000100 10 
+echo 1111 10 010 01 0010100 10 01001010 1 11 0001 00 001 1 1 01 0100000 1 1 11 1 
+echo 000 1000 10 010 100 0010 10 01 010 010 10000 010 010 01 10010010010 1001 100 
+echo 1010010 010 100 010 10 010 10 010 1010 010 01 01 00001 010 01 010 010 10 001 
+echo 111 11111 11111 11111 11111 11111 1111111 111111111 
+echo 101010100 100 00100 010 0010 01000010 010 01000100010 01000 0100100 100 011 
+echo 1111 10 010 01 0010100 10 01001010 1 11 0001 00 001 1 1 01 0100000 1 1 11 1 
+echo 000 1000 10 010 100 0010 10 01 010 010 10000 010 010 01 10010010010 1001 10011 
+echo 1010010 010 100 010 10 010 10 010 1010 010 01 01 00001 010 01 010 010 10 0011111 
+echo 101 0100010 1000001 01010 10 0100010 10 0010 010010 0 010 010 00001110010110111 
+echo 000 010 01010010 010001001 00100100 1010 01001001 0010010 01001000100 001001111 
+echo 0000 00 000000000000 000000000000000000 00000000000001111 10 0100000 1000000111 
+echo 111111111111111111111111111111111111111111111111111111111111111111111111111110
+echo 001110010101010101011110000101010101010011000110010101010101010100101010101011
+echo %random% %random% %random% %random% %random% %random% %random% %random% %random%
+goto start 
