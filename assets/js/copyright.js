d73cbdcde535d64278128f8c1ac0b0e99b5edd11
~$  The Coder ( nixton ) <83667327+nixtondev@users.noreply.github.com>
2023-06-21 11:34:53 +0200

    Delete assets/js directory

--- assets/js/copyright.js
+++
@@ -1,9 +0,0 @@
-var copy = "&copy;";
-var author = " nixton.dev";
-
-function getCopy(year){
-document.write(copy);
-document.write(author);
-document.write(year, ' - ');
-document.write(new Date().getFullYear());
-}
