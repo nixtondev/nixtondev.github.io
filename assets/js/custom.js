9ae2f05a139f71fc424d07a3220e5163d2c6f7a2
~$  The Coder ( nixton ) <83667327+nixtondev@users.noreply.github.com>
2023-07-06 11:37:25 +0200

    Update custom

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,3 +1,9 @@
+var getAge = "13/02/1997".split("/");
+var birthDate = new Date(getAge[2], getAge[1] - 1, getAge[0]);
+var today = new Date();
+var diff = today - birthDate; // Difference in milliseconds
+var myAge = Math.floor(diff/31557600000); // Divide by 1000*60*60*24*365.25
+document.getElementsByClassName("age")[0].innerHTML = myAge;
 
 const currentYear = new Date().getFullYear();
 const copyrightText = "&copy; nixton.dev 2020 - ${currentYear} ";
