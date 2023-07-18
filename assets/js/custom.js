6490c29f7e0ba30d6807fe7367f03b7b78360370
~$  The Coder ( nixton ) <83667327+nixtondev@users.noreply.github.com>
2023-07-18 23:35:22 +0200

    Update file

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -5,7 +5,7 @@ function getCopy(year){
   
 document.write(copy);
 document.write(author);
-//document.write(year, ' - ');
+document.write(year, ' - ');
 document.write(new Date().getFullYear());
   
 }
