a670fd946730b8d7b37e8428a4c1783bb46005f4
~$  The Coder ( nixton ) <83667327+nixtondev@users.noreply.github.com>
2023-11-10 09:05:11 +0100

    Update stuff

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -9,28 +9,3 @@
         document.write(new Date().getFullYear());
   
         }
-
-        const darkModeToggle = document.getElementById("darkModeToggle");
-        const body = document.body;
-
-        if (localStorage.getItem("darkMode") === "enabled") {
-            enableDarkMode();
-        }
-
-        darkModeToggle.addEventListener("click", () => {
-            if (body.classList.contains("dark-mode")) {
-                disableDarkMode();
-            } else {
-                enableDarkMode();
-            }
-        });
-
-        function enableDarkMode() {
-            body.classList.add("dark-mode");
-            localStorage.setItem("darkMode", "enabled");
-        }
-
-        function disableDarkMode() {
-            body.classList.remove("dark-mode");
-            localStorage.setItem("darkMode", "disabled");
-        }
