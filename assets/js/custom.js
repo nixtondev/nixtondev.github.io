bbc77c60d3a9740a88860e0ef645ab79ffe72459
~$  The Coder ( nixton ) <83667327+nixtondev@users.noreply.github.com>
2023-11-10 08:59:13 +0100

    Add files via upload

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,11 +1,36 @@
-var copy = "&copy;";
-var author = " nixton.dev ";
+        var copy = "&copy;";
+        var author = " nixton.dev ";
 
-function getCopy(year){
+        function getCopy(year){
   
-document.write(copy);
-document.write(author);
-document.write(year, ' - ');
-document.write(new Date().getFullYear());
+        document.write(copy);
+        document.write(author);
+        document.write(year, ' - ');
+        document.write(new Date().getFullYear());
   
-}
+        }
+
+        const darkModeToggle = document.getElementById("darkModeToggle");
+        const body = document.body;
+
+        if (localStorage.getItem("darkMode") === "enabled") {
+            enableDarkMode();
+        }
+
+        darkModeToggle.addEventListener("click", () => {
+            if (body.classList.contains("dark-mode")) {
+                disableDarkMode();
+            } else {
+                enableDarkMode();
+            }
+        });
+
+        function enableDarkMode() {
+            body.classList.add("dark-mode");
+            localStorage.setItem("darkMode", "enabled");
+        }
+
+        function disableDarkMode() {
+            body.classList.remove("dark-mode");
+            localStorage.setItem("darkMode", "disabled");
+        }
